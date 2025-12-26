12a37b63aebb67cf06df0f8c5d7ccd88a2ffa961
Yonathan967 <yonathanp993@gmail.com>
2025-12-26 21:53:20 +0700

    perbaikan minor

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,7 +3,7 @@
 
 <aside class="sidebar" style="background-image: url('{{ url('/halaman_depan/images/background.jpeg') }}')"></aside>
 <div class="sidebar-logo">
-        <img src="{{ url('/halaman_depan/images/icons/logo_reece.ico') }}" alt="Logo">
+    <img src="{{ asset('halaman_depan/images/icons/logo_reece.ico') }}" class="logo" alt="Logo Reece Farms">
         
     </div>
 
